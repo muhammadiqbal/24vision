a644c41e23e9b825f55a9955ce7721a6f584b152
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-02 23:20:10 +0100

    asd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,11 +67,11 @@
             </div>
             <div class="row">
                 <div class="col-xs-8">
-                    <div class="checkbox icheck">
+                    {{-- <div class="checkbox icheck">
                         <label>
                             <input type="checkbox" name="remember"> Remember Me
                         </label>
-                    </div>
+                    </div> --}}
                 </div>
                 <!-- /.col -->
                 <div class="col-xs-4">
