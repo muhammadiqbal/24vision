074d2b45b98c11020fbf177765deb1095ab27f3b
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-03 13:35:26 +0100

    asd

--- resources/views/calculator/datatables_actions.blade.php
+++ resources/views/calculator/datatables_actions.blade.php
@@ -2,7 +2,7 @@
     <a class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-list-alt"></i>
     </a>
-    <a href="{{url('/voyage/'.$ship.'/'.$id.'/'.$port.'/'.$date_of_opening)}}" class='btn btn-default btn-xs'>
+    <a href="{{url('/voyage/'.$ship.'/'.$cargo->id.'/'.$port.'/'.$date_of_opening)}}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-search"></i>
     </a>
     <a href="{{url('/emails/'.$emailID)}}" class='btn btn-default btn-xs'>
