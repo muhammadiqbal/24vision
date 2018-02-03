069a5e16067712ce3969050f82641b02017047a0
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-03 13:36:10 +0100

    asd

--- resources/views/calculator/datatables_actions.blade.php
+++ resources/views/calculator/datatables_actions.blade.php
@@ -5,13 +5,13 @@
     <a href="{{url('/voyage/'.$ship.'/'.$cargo->id.'/'.$port.'/'.$date_of_opening)}}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-search"></i>
     </a>
-    <a href="{{url('/emails/'.$emailID)}}" class='btn btn-default btn-xs'>
+    <a href="{{url('/emails/'.$cargo->emailID)}}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-envelope" data-toggle="modal" data-target="#modalMail"></i>
     </a>
     <a class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-warning-sign"></i>
     </a>
-    <a href="{{ route('cargos.edit', $id) }}" class='btn btn-default btn-xs'>
+    <a href="{{ route('cargos.edit', $cargo->id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-share"></i>
     </a>
 </div>
