bbad2e36781c5d62e5a3acccc5a98a51d83172ba
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-03 11:25:03 +0100

    asd

--- resources/views/emails/datatables_actions.blade.php
+++ resources/views/emails/datatables_actions.blade.php
@@ -1,9 +1,9 @@
-{!! Form::open(['route' => ['emails.destroy', $id], 'method' => 'delete']) !!}
+{!! Form::open(['route' => ['emails.destroy', $emailID], 'method' => 'delete']) !!}
 <div class='btn-group'>
-    <a href="{{ route('emails.show', $id) }}" class='btn btn-default btn-xs'>
+    <a href="{{ route('emails.show', $emailID) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
-    <a href="{{ route('emails.edit', $id) }}" class='btn btn-default btn-xs'>
+    <a href="{{ route('emails.edit', $emailID) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-edit"></i>
     </a>
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
