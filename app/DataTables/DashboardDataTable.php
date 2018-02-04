abd41aef7ee1b3c76616a67c99ef5a0bfa301f6c
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-04 16:59:17 +0100

    asd

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -61,12 +61,13 @@ class DashboardDataTable extends DataTable
 
         return datatables()
             ->eloquent($this->query())
-            ->rawColumn('bdi', function(Cargo $cargo){
-                $distance_to_start = $this->calculator->calculateDistancetoStart($this->port, $cargo->id, $this->calculator);
-                $travel_time_to_start = $this->calculator->calculateTravelTimeToStart($this->ship, $distance_to_start);
-                $bdi_id = $this->calculator->calculateBDIId($this->port,$cargo->id);
-                $bdi = $this->calculator->calculateBDI($bdi_id, $this->date_of_opening, $travel_time_to_start);
-                return $bdi;
+            ->addColumn('bdi', function(Cargo $cargo){
+                // $distance_to_start = $this->calculator->calculateDistancetoStart($this->port, $cargo->id, $this->calculator);
+                // $travel_time_to_start = $this->calculator->calculateTravelTimeToStart($this->ship, $distance_to_start);
+                // $bdi_id = $this->calculator->calculateBDIId($this->port,$cargo->id);
+                // $bdi = $this->calculator->calculateBDI($bdi_id, $this->date_of_opening, $travel_time_to_start);
+                // return $bdi;
+                return 'asd';
             })
             // ->addColumn('ntce', function(Cargo $cargo){
             //     $ship_bdi = Ship::first();
