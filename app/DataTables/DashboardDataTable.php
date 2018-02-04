2858707c7343aab5fd921110f714dffc7bf406b6
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-04 18:35:49 +0100

    asd

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -142,7 +142,7 @@ class DashboardDataTable extends DataTable
                         ->leftjoin('cargo_types', 'cargos.cargo_type_id','cargo_types.id')
                         ->leftjoin('ports as p1', 'p1.id','loading_port')
                         ->leftjoin('ports as p2', 'p2.id','discharging_port')
-                        ->where('quantity','<=', ($this->ship->dwcc - $this->occupied_tonage));
+                        ->where('quantity','<=', ($this->ship->dwcc - $this->occupied_tonage))
                         // ->where(DB::raw('quantity * stowage_factor AS size'),
                         //                 '<=',
                         //                 ($this->ship->max_holds_capacity - $this->occupied_size))
