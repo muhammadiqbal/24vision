d1eed3fd550e96c99a5746c7817c04b4eb1062f6
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-06 17:12:33 +0100

    asd

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -53,7 +53,7 @@ class DashboardDataTable extends DataTable
                 $port = $this->port;
                 $date_of_opening = $this->date_of_opening;
                 //waiting for debugging calculator in model
-                $bdi = $cargo->setBdi($port, $ship, $date_of_opening);
+                //$bdi = $cargo->setBdi($port, $ship, $date_of_opening);
                 
                 return view('calculator.bdi', 
                         compact('bdi'))->render();
