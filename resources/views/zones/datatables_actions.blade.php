8e43d0ac40cd9e78184856ce63206f2235ed8c5f
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-06 15:03:41 +0100

    asd

--- resources/views/zones/datatables_actions.blade.php
+++ resources/views/zones/datatables_actions.blade.php
@@ -1,6 +1,5 @@
 {!! Form::open(['route' => ['zones.destroy', $id], 'method' => 'delete']) !!}
 <div class='btn-group'>
-    
     <a href="{{ route('zones.show', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
--- resources/views/zones/show.blade.php
+++ resources/views/zones/show.blade.php
@@ -11,6 +11,11 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('zones.show_fields')
+                    <h1>Zone Point</h1>
+                    @foreach($zone->zonePoints() as $point)
+                        <p>latitude: <b>{{$point->latitude}}</b></p>
+                        <p>longitude: <b>{{$point->longitude}}</b></p>
+                    @endforeach
                     <a href="{!! route('zones.index') !!}" class="btn btn-default">Back</a>
                 </div>
             </div>
