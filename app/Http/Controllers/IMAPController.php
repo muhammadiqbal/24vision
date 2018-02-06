4165488735523ab0b201ca78136817e6b91b1967
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-06 15:24:56 +0100

    asd

--- app/Http/Controllers/IMAPController.php
+++ app/Http/Controllers/IMAPController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PhpImap\Mailbox;
-use App\Http\Requests\CreateEmailRequest;
+use App\Repositories\EmailRepository;
 
 class IMAPController extends Controller
 {
