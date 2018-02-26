d3164419db9ca47847d5176f8acfa152840a3570
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-26 16:57:36 +0100

    ert

--- app/Http/Controllers/DistanceController.php
+++ app/Http/Controllers/DistanceController.php
@@ -60,7 +60,7 @@ class DistanceController extends AppBaseController
         $startPort = Port::find($request->get('start_port'));
         $endPort = Port::find($request->get('end_port'));
 
-        $script = 'cd ~/anaconda3/envs/laravelenv/bin/ && python3 /var/www/24vision/PyTools/DistanceCalculator.py '.$startPort->id.' '.$endPort->id.' '.$startPort->latitude.' '.$startPort->longitude.' '.$endPort->latitude.' '.$endPort->longitude;
+        $script = 'cd /anaconda3/envs/laravelenv/bin/ && python3 /var/www/24vision/PyTools/DistanceCalculator.py '.$startPort->id.' '.$endPort->id.' '.$startPort->latitude.' '.$startPort->longitude.' '.$endPort->latitude.' '.$endPort->longitude;
 
         $process = new Process($script);
         $process->run();
