8e8e8b1de44cc3e535109d84394dec674f45ab40
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-27 09:19:01 +0100

    test

--- resources/views/calculator/datatables_actions.blade.php
+++ resources/views/calculator/datatables_actions.blade.php
@@ -1,15 +1,15 @@
 <div class='btn-group'>
 
-    <a href="{{url('/voyage/'.$ship->id.'/'.$cargo->id.'/'.$port->id.'/'.$date_of_opening)}}" target="_blank" class='btn btn-default btn-xs'>
+    <a href="{{url('/voyage/'.$ship->id.'/'.$cargo->id.'/'.$port->id.'/'.$date_of_opening)}}" target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Voyage">
         <i class="glyphicon glyphicon-search"></i>
     </a>
-    <a href="{{url('/emails/'.$cargo->email_id)}}"  target="_blank" class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-envelope" data-toggle="modal" data-target="#modalMail"></i>
+    <a href="{{url('/emails/'.$cargo->email_id)}}"  target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Mail">
+        <i class="glyphicon glyphicon-envelope"></i>
     </a>
-    <a class='btn btn-default btn-xs'>
-        <i class="glyphicon glyphicon-warning-sign"></i>
+    <a class='btn btn-default btn-xs' data-toggle="tooltip" title="forward to VMS">
+        <i class="glyphicon glyphicon-share-alt"></i>
     </a>
-    <a href="{{ route('cargos.edit', $cargo->id) }}" class='btn btn-default btn-xs'>
+    <a href="{{ route('cargos.edit', $cargo->id) }}" class='btn btn-default btn-xs' data-toggle="tooltip" title="Edit">
         <i class="glyphicon glyphicon-share"></i>
     </a>
 </div>
--- resources/views/cargos/fields.blade.php
+++ resources/views/cargos/fields.blade.php
@@ -1,18 +1,22 @@
 <!-- Loading Port Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('loading_port', 'Loading Port:') !!}
-    {!! Form::number('loading_port', null, ['class' => 'form-control']) !!}
+    <select name="loading_port" class="form-control">
+        @foreach($ports as $port)
+            <option value="{{$port->id}}">{{$port->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Discharging Port Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('discharging_port', 'Discharging Port:') !!}
-    {!! Form::number('discharging_port', null, ['class' => 'form-control']) !!}
+    <select name="discharging_port" class="form-control">
+        @foreach($ports as $port)
+            <option value="{{$port->id}}">{{$port->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Laycan First Day Field -->
 <div class="form-group col-sm-6">
@@ -20,23 +24,22 @@
     {!! Form::date('laycan_first_day', null, ['class' => 'form-control']) !!}
 </div>
 
-
-
 <!-- Laycan Last Day Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('laycan_last_day', 'Laycan Last Day:') !!}
     {!! Form::date('laycan_last_day', null, ['class' => 'form-control']) !!}
 </div>
 
-
-
 <!-- Cargo Type Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('cargo_type_id', 'Cargo Type Id:') !!}
     {!! Form::number('cargo_type_id', null, ['class' => 'form-control']) !!}
+    <select name="cargo_type_id" class="form-control">
+        @foreach($cargo_types as $cargo_type)
+            <option value="{{$cargo_type->id}}">{{$cargo_type->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Stowage Factor Field -->
 <div class="form-group col-sm-6">
@@ -44,28 +47,34 @@
     {!! Form::number('stowage_factor', null, ['class' => 'form-control']) !!}
 </div>
 
-
-
 <!-- Sf Unit Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('sf_unit', 'Sf Unit:') !!}
-    {!! Form::number('sf_unit', null, ['class' => 'form-control']) !!}
+    <select name="sf_unit" class="form-control">
+        @foreach($sf_units as $sf_unit)
+            <option value="{{$sf_unit->id}}">{{$sf_unit->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Ship Specialization Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('ship_specialization_id', 'Ship Specialization Id:') !!}
-    {!! Form::number('ship_specialization_id', null, ['class' => 'form-control']) !!}
+    <select name="ship_specialization_id" class="form-control">
+        @foreach($ship_specializations as $ship_specialization)
+            <option value="{{$ship_specialization->id}}">{{$ship_specialization->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Quantity Measurement Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('quantity_measurement_id', 'Quantity Measurement Id:') !!}
-    {!! Form::number('quantity_measurement_id', null, ['class' => 'form-control']) !!}
+    <select name="quantity_measurement_id" class="form-control">
+        @foreach($quantity_measurements as $quantity_measurement)
+            <option value="{{$quantity_measurement->id}}">{{$quantity_measurement->name}}</option>
+        @endforeach
+    </select>
 </div>
 
 <!-- Quantity Field -->
@@ -77,7 +86,11 @@
 <!-- Loading Rate Type Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('loading_rate_type', 'Loading Rate Type:') !!}
-    {!! Form::number('loading_rate_type', null, ['class' => 'form-control']) !!}
+    <select name="loading_rate_type" class="form-control">
+        @foreach($ld_rate_types as $ld_rate_type)
+            <option value="{{$ld_rate_type->id}}">{{$ld_rate_type->name}}</option>
+        @endforeach
+    </select>
 </div>
 
 
@@ -87,12 +100,15 @@
     {!! Form::number('loading_rate', null, ['class' => 'form-control']) !!}
 </div>
 
-
-
 <!-- Discharging Rate Type Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('discharging_rate_type', 'Discharging Rate Type:') !!}
     {!! Form::number('discharging_rate_type', null, ['class' => 'form-control']) !!}
+    <select name="discharging_rate_type" class="form-control">
+        @foreach($ld_rate_types as $ld_rate_type)
+            <option value="{{$ld_rate_type->id}}">{{$ld_rate_type->name}}</option>
+        @endforeach
+    </select>
 </div>
 
 
@@ -102,14 +118,11 @@
     {!! Form::number('discharging_rate', null, ['class' => 'form-control']) !!}
 </div>
 
-
-
 <!-- Extra Condition Field -->
 <div class="form-group col-sm-12 col-lg-12">
     {!! Form::label('extra_condition', 'Extra Condition:') !!}
     {!! Form::textarea('extra_condition', null, ['class' => 'form-control']) !!}
 </div>
-
 
 <!-- Comission Field -->
 <div class="form-group col-sm-6">
@@ -123,14 +136,15 @@
     {!! Form::number('email_id', null, ['class' => 'form-control']) !!}
 </div>
 
-
 <!-- Status Id Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('status_id', 'Status Id:') !!}
-    {!! Form::number('status_id', null, ['class' => 'form-control']) !!}
+    <select name="status_id" class="form-control">
+        @foreach($cargo_statuses as $cargo_status)
+            <option value="{{$cargo_status->id}}">{{$cargo_status->name}}</option>
+        @endforeach
+    </select>
 </div>
-
-
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
