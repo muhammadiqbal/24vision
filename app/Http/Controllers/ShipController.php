8522aefb4146fbb21168a2f3b1b4aa168a6c1159
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-27 13:25:54 +0100

    test

--- app/Http/Controllers/ShipController.php
+++ app/Http/Controllers/ShipController.php
@@ -7,6 +7,7 @@ use App\Http\Requests;
 use App\Http\Controllers\AppBaseController;
 use App\Http\Requests\CreateShipRequest;
 use App\Http\Requests\UpdateShipRequest;
+use App\Models\FuelType;
 use App\Models\ShipType;
 use App\Models\ShipSpecialization;
 use App\Repositories\ShipRepository;
