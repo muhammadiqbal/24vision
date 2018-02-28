8a9551a009f3c560b0e1180742fdbe2a2838fefd
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-02-28 03:10:32 +0100

    test

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -126,7 +126,8 @@ class DashboardDataTable extends DataTable
      */
     public function query()
     {
-        $cargo = Cargo::leftjoin('cargo_status', 'cargo_status.id','cargo_status.id')
+        $cargo = DB::table('cargos')
+                        ->leftjoin('cargo_status', 'cargo_status.id','cargo_status.id')
                         ->leftjoin('cargo_types', 'cargos.cargo_type_id','cargo_types.id')
                         ->leftjoin('ports as p1', 'p1.id','loading_port')
                         ->leftjoin('ports as p2', 'p2.id','discharging_port')
