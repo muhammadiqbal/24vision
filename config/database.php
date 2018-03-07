6243382517984dc97bf6ccf02849ac72080046e7
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-08 00:29:53 +0100

    test

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql2'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
