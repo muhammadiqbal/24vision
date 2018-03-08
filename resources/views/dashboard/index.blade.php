bdfd16c69c2e2e1186a00b70f26dd06f53574f81
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-08 14:18:19 +0100

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,9 +4,9 @@
 	 <div class="content">        
         <div class="box box-primary">
             <div class="box-body">
-				<div class="fee-div"></div>
-				<div class="fuel-div"></div>
-				<div class="bdi-div"></div>
+				<div id="fee-div"></div>
+				<div id="fuel-div"></div>
+				<div id="bdi-div"></div>
 				<?php $feePriceChart->render('LineChart', 'feePricedata', 'fee-div'); ?>
 				<?php $fuelPriceChart->render('LineChart', 'fuelPricedata', 'fuel-div'); ?>
 				<?php $bdiPriceChart->render('LineChart', 'bdiPricedata', 'bdi-div'); ?>
