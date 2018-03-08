fdd6d162eaec3f97bb70c3a889cb124f21071f39
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-08 14:16:42 +0100

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="fee-div"></div>
-	<div class="fuel-div"></div>
-	<div class="bdi-div"></div>
-	<?php $feePriceChart->render('LineChart', 'feePricedata', 'fee-div'); ?>
-	<?php $fuelPriceChart->render('LineChart', 'fuelPricedata', 'fuel-div'); ?>
-	<?php $bdiPriceChart->render('LineChart', 'bdiPricedata', 'bdi-div'); ?>
+	 <div class="content">        
+        <div class="box box-primary">
+            <div class="box-body">
+				<div class="fee-div"></div>
+				<div class="fuel-div"></div>
+				<div class="bdi-div"></div>
+				<?php $feePriceChart->render('LineChart', 'feePricedata', 'fee-div'); ?>
+				<?php $fuelPriceChart->render('LineChart', 'fuelPricedata', 'fuel-div'); ?>
+				<?php $bdiPriceChart->render('LineChart', 'bdiPricedata', 'bdi-div'); ?>
+			</div>
+		</div>
+	</div>
 @endsection
