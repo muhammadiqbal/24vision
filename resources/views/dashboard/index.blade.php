3d7f0a9d7a7f3c5c64096f0da2bcc4284427788d
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-08 13:26:39 +0100

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-	
+	<div class="fee-div"></div>
+	<div class="fuel-div"></div>
+	<div class="bdi-div"></div>
 	<?php $feePriceChart->render('LineChart', 'feePriceChart', 'fee-div'); ?>
 	<?php $fuelPriceChart->render('LineChart', 'fuelPriceChart', 'fuel-div'); ?>
 	<?php $bdiPriceChart->render('LineChart', 'bdiPriceChart', 'bdi-div'); ?>
