f7f5d4a072445858ce2b7560de0fa125303d11fb
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-08 15:32:41 +0100

    test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -123,7 +123,7 @@ class DashboardController extends Controller
         // $bdiPricedata = $bdiPriceChart->DataTable();
         // $fuelPricedata = $fuelPriceChart->DataTable();
 
-        $feePrices = FeePrice::select('start_date as x', 'price as y')
+        $feePrices = FeePrice::select('null as x','null as y','start_date', 'price')
                               ->where('port_id', $port)
                               ->get()
                               ->toJson();
