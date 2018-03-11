92d2aa2ac0db7aa020c86e7660d37dff953cdd8b
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-11 22:36:23 +0100

    test

--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -48,8 +48,10 @@
             </div>
 
             <div class="col-sm-3 col-xs-12">
-                
+                <div class="info-box" style="border-radius: 100% 100% 50% 50%">
                   <a href="{{url('cargoMap')}}" target="_blank"><span class="info-box-icon bg-red" style="border-radius:100%;"><i class="fa fa-globe" data-toggle="tooltip" title="View cargo Map"></i></span></a>
+                  <span class="info-box-text">Ship</span>
+                </div>
                
             </div>
 
