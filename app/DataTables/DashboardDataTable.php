b938d1b33db48e936c7804552740d464c4f52c42
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-19 10:55:53 +0100

    debugging

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -157,7 +157,7 @@ class DashboardDataTable extends DataTable
                             ->where(function($q){
                                 if($this->request()->get('range')){
                                     $q->where('loading_port',$this->port->id);
-                                    $q->orHaving('range','<=',$this->request()->get('range'));
+                                    $q->orHaving('range','g<=',$this->request()->get('range'));
                                 }
                             });
                             ->where('quantity','<=',  $this->remaining_tonage)
