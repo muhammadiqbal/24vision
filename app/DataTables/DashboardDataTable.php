24cd9c0faaf6651f895ef6d82b9e5551f09bc823
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-20 11:57:24 +0100

    test

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -171,10 +171,10 @@ class DashboardDataTable extends DataTable
         }
         if($this->request()->get('date_of_opening')){
             $cargo->where(function($q){
-                $q->whereDate('laycan_first_day','<=',$this->request()->get('date_of_opening'));
-                $q->whereDate('laycan_last_day','>=',$this->request()->get('date_of_opening'));
+                $q->where('laycan_first_day','<=',$this->request()->get('date_of_opening'));
+                $q->where('laycan_last_day','>=',$this->request()->get('date_of_opening'));
                 $q->orWhere(function($q){
-                    $q->whereDate('laycan_first_day','<=',$this->request()->get('date_of_opening'));
+                    $q->where('laycan_first_day','<=',$this->request()->get('date_of_opening'));
                     $q->whereNull('laycan_last_day');
                 });
             });
