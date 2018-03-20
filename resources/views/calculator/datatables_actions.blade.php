79aee41f8264acbb7622633234a080e3c060ec86
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-20 14:37:58 +0100

    test

--- resources/views/calculator/datatables_actions.blade.php
+++ resources/views/calculator/datatables_actions.blade.php
@@ -1,18 +1,19 @@
 <div class='btn-group' style="display: inline-flex;">
-
+@if(isset($cargo))
     <a href="{{url('/voyage/'.$ship->id.'/'.$cargo->id.'/'.$port->id.'/'.$date_of_opening)}}" target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Voyage">
         <i class="glyphicon glyphicon-search"></i>
     </a>
-    @if(isset($cargo->email_id))
+    
     <a href="{{url('/emails/'.$cargo->email_id)}}"  target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Mail">
         <i class="glyphicon glyphicon-envelope"></i>
     </a>
-    @endif
+    
     <a class='btn btn-default btn-xs' data-toggle="tooltip" title="forward to VMS">
         <i class="glyphicon glyphicon-share-alt"></i>
     </a>
     <a href="{{ route('cargos.edit', $cargo->id) }}" class='btn btn-default btn-xs' data-toggle="tooltip" title="Edit">
         <i class="glyphicon glyphicon-share"></i>
     </a>
+@endif
 </div>
 
