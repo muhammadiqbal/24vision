a1ab623a9a76aa53fa9a50b89d33fbc4e5703280
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-20 15:04:41 +0100

    test

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -154,7 +154,7 @@ class DashboardDataTable extends DataTable
                                       'p1.name as load_port',
                                       'p2.name as disch_port',
                                       DB::raw('(quantity / '.$this->ship->dwcc.')*'.$this->ship->max_laden_draft - $this->ship->ballast_draft.' AS draft'),
-                                      DB::raw('quantity * cargos.stowage_factor AS size'),
+                                      DB::raw('quantity * cargo_types.stowage_factor AS size'),
                                       DB::raw('ST_Distance(POINT('.$this->port->latitude.','.$this->port->longitude.'), POINT(p1.latitude,p1.longitude)) AS \'range\'' )
                                     ])
                             ->leftjoin('cargo_status', 'cargos.status_id','cargo_status.id')
