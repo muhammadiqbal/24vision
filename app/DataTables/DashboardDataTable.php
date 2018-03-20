65ad4a9aa203332b2ec5ded5ca814911ee2ee74f
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-20 10:07:54 +0100

    test

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -156,7 +156,7 @@ class DashboardDataTable extends DataTable
                             ->leftjoin('ports as p1', 'p1.id','loading_port')
                             ->leftjoin('ports as p2', 'p2.id','discharging_port')
                             //->where('loading_port',$this->port->id);
-                            ->having('range','<=',$this->request()->get('range')? $this->request()->get('range'):0);
+                            ->having('range','<=',$this->request()->get('range')? $this->request()->get('range'):0)
                             ->where('quantity','<=', $this->remaining_tonage)
                             ->having('size','<=',$this->remaining_size)
                             ->having('draft','<=',$this->remaining_draft);
