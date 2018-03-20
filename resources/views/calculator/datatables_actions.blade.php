7a29a6ffc87b8cb243f7dd470732a21fdc5878fc
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-20 14:33:56 +0100

    test

--- resources/views/calculator/datatables_actions.blade.php
+++ resources/views/calculator/datatables_actions.blade.php
@@ -3,9 +3,11 @@
     <a href="{{url('/voyage/'.$ship->id.'/'.$cargo->id.'/'.$port->id.'/'.$date_of_opening)}}" target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Voyage">
         <i class="glyphicon glyphicon-search"></i>
     </a>
+    @if(isset($cargo->email_id))
     <a href="{{url('/emails/'.$cargo->email_id)}}"  target="_blank" class='btn btn-default btn-xs' data-toggle="tooltip" title="View Mail">
         <i class="glyphicon glyphicon-envelope"></i>
     </a>
+    @endif
     <a class='btn btn-default btn-xs' data-toggle="tooltip" title="forward to VMS">
         <i class="glyphicon glyphicon-share-alt"></i>
     </a>
