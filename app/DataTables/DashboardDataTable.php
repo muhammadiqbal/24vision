5b96c28885ea4055ed35ce910711d4d9e5c953d0
Muhammad Iqbal <iqbalms.iqbal@gmail.com>
2018-03-21 09:00:40 +0100

    test

--- app/DataTables/DashboardDataTable.php
+++ app/DataTables/DashboardDataTable.php
@@ -170,7 +170,7 @@ class DashboardDataTable extends DataTable
             $cargo->where(function($q){
                 $q->whereDate('laycan_first_day','<=',$this->request()->get('date_of_opening'));
                 $q->orWhere(function($q){
-                    $q->whereDate('laycan_first_day','<=',$this->request()->get('date_of_opening'));
+                    $q->whereDate('laycan_last_day','>=',$this->request()->get('date_of_opening'));
                     $q->whereNull('laycan_last_day');
                 });
             });
